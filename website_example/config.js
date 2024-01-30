b35863174469fca69dd584da2126d6b1c889d454
Infinium-8 <132166430+Infinium-8-Developers@users.noreply.github.com>
2024-01-30 23:31:21 +0100

    init

---
+++ website_example/config.js
@@ -0,0 +1,12 @@
+var api = "https://api.pool.infinium.space";
+let parentCoin = "Infinium-8"
+
+var email = "support@poolhost.com";
+var telegram = "https://t.me/YourPool";
+var discord = "https://discordapp.com/invite/YourPool";
+var facebook = "https://www.facebook.com/<YourPoolFacebook>";
+
+var marketCurrencies = ["{symbol}-BTC", "{symbol}-LTC", "{symbol}-DOGE", "{symbol}-USDT", "{symbol}-USD", "{symbol}-EUR", "{symbol}-CAD"];
+
+var themeCss = "themes/default.css";
+var defaultLang = 'en';
